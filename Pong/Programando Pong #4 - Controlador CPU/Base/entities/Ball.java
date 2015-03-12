b455bc9b051a00261d9f97d4c9d7cd5f73fb7600
JCabero <j.c.revolution@hotmail.com>
2015-03-12 13:34:24 +0100

    Añadido código controlador CPU!

---
+++ Pong/Programando Pong #4 - Controlador CPU/Base/entities/Ball.java
@@ -0,0 +1,64 @@
+package entities;
+
+import java.util.Random;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.Renderable;
+
+import core.Main;
+
+import util.Updatable;
+
+public class Ball implements Updatable, Renderable {
+
+	private float x, y, 
+    			  w, h, 
+    			  dx, dy,
+    			  default_speed = 5;
+    
+	public Ball(float x, float y, float w, float h) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		
+		Random r = new Random();
+		
+		// Random x speed
+		if(r.nextBoolean())
+			dx =  default_speed;
+		else
+			dx = -default_speed;
+		
+		// Random y speed
+		if(r.nextBoolean())
+			dy =  default_speed;
+		else
+			dy = -default_speed;
+	}
+	
+	public void render() {
+		GL11.glBegin(GL11.GL_QUADS);
+			GL11.glVertex2f(x,   y);
+			GL11.glVertex2f(x,   y+h);
+			GL11.glVertex2f(x+w, y+h);
+			GL11.glVertex2f(x+w, y);
+		GL11.glEnd();
+	}
+
+	public void update() {
+		
+		// Update ball position
+		x += dx;
+		y += dy;
+		
+		// Horizontal screen collision
+		if(x < 0 || x+w > Main.SCREEN_WIDTH)
+			dx *= -1;
+		
+		// Vertical   screen collision
+		if(y < 0 || y+h > Main.SCREEN_HEIGHT)
+			dy *= -1;
+	}
+
+}
